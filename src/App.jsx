1265578f7f5cb810a69c32bb6b0198cb1b3dcc4f
allenlobo31 <allenlobo05@gmail.com>
2026-03-24 21:02:48 +0530

    build

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Component from './component/component.jsx'
+import './App.css'
+import Funk1 from './component/hooks.jsx'
+import { useState } from 'react';
+import Navbar from './component/navbar.jsx';
+
+function App() {
+
+
+  return (
+
+    <>
+
+
+<Navbar />
+
+     
+    </>
+  )
+}
+
+export default App
